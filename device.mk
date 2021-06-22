Valera1978/android_device_samsung_gtaxlwifi

gtaxl: Do not copy fstab to /vendor
- first stage init requires fstab in ramdisk root and /vendor/etc/ - but not in /vendor root.
- we already copy fstab to /vendor/etc in rootdir/Android.mk

device.mk
@@ -44,8 +44,7 @@ TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # first_stage_mount
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/fstab.samsungexynos7870:$(TARGET_COPY_OUT_RAMDISK)/fstab.samsungexynos7870 \
-    $(LOCAL_PATH)/rootdir/fstab.samsungexynos7870:$(TARGET_COPY_OUT_VENDOR)/fstab.samsungexynos7870
+    $(LOCAL_PATH)/rootdir/fstab.samsungexynos7870:$(TARGET_COPY_OUT_RAMDISK)/fstab.samsungexynos7870
 
 # Ramdisk
 PRODUCT_PACKAGES += \

MiDoNaSR545/android_device_samsung_gtaxllte

device.mk
@@ -41,8 +41,7 @@ TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
 # first_stage_mount
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ramdisk/fstab.samsungexynos7870:$(TARGET_COPY_OUT_RAMDISK)/fstab.samsungexynos7870 \
-    $(LOCAL_PATH)/ramdisk/fstab.samsungexynos7870:$(TARGET_COPY_OUT_VENDOR)/fstab.samsungexynos7870
+    $(LOCAL_PATH)/ramdisk/fstab.samsungexynos7870:$(TARGET_COPY_OUT_RAMDISK)/fstab.samsungexynos7870
 
 # Ramdisk
 PRODUCT_PACKAGES += \